0042d65f2d3aced07c28abd7e9b81d80846d3254
migueldosantos1 <miguelhcs.2005@gmail.com>
2026-06-03 11:34:44 +0100

    output files and new sh file

--- project-OOP-2526/DEMO-v1/phase1-script.sh
+++ project-OOP-2526/DEMO-v1/phase1-script.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # create the output folder if needed
-mkdir -p output
+mkdir -p DEMO-v1/output
 
 echo "=================================================="
 echo "Example 1"
 echo "=================================================="
-java -jar project-v1.jar in-out-files-for-demonstration/input/ex1-deck.txt in-out-files-for-demonstration/input/ex1-script.txt 4 > output/out1.txt
+java -jar project-v1.jar DEMO-v1/input/ex1-deck.txt DEMO-v1/input/ex1-script.txt 2 > DEMO-v1/output/out1.txt
 
 echo "=================================================="
 echo "Example 2"
 echo "=================================================="
-java -jar project-v1.jar in-out-files-for-demonstration/input/ex2-deck.txt in-out-files-for-demonstration/input/ex2-script.txt 4 > output/out2.txt
+java -jar project-v1.jar DEMO-v1/input/ex2-deck.txt DEMO-v1/input/ex2-script.txt 3 > DEMO-v1/output/out2.txt
 
 echo "=================================================="
 echo "Example 3"
 echo "=================================================="
-java -jar project-v1.jar in-out-files-for-demonstration/input/ex3-deck.txt in-out-files-for-demonstration/input/ex3-script.txt 4 > output/out3.txt
+java -jar project-v1.jar DEMO-v1/input/ex3-deck.txt DEMO-v1/input/ex3-script.txt 4 > DEMO-v1/output/out3.txt
 
 echo "=================================================="
 echo "Example 4"
 echo "=================================================="
-java -jar project-v1.jar in-out-files-for-demonstration/input/ex4-deck.txt in-out-files-for-demonstration/input/ex4-script.txt 4 > output/out4.txt
+java -jar project-v1.jar DEMO-v1/input/ex4-deck.txt DEMO-v1/input/ex4-script.txt 5 > DEMO-v1/output/out4.txt
 
 echo "=================================================="
 echo "Example 5"
 echo "=================================================="
-java -jar project-v1.jar in-out-files-for-demonstration/input/ex5-deck.txt in-out-files-for-demonstration/input/ex5-script.txt 4 > output/out5.txt
+java -jar project-v1.jar DEMO-v1/input/ex5-deck.txt DEMO-v1/input/ex5-script.txt 6 > DEMO-v1/output/out5.txt
 
 echo "=================================================="
 echo "All done! Outputs saved in output/"
