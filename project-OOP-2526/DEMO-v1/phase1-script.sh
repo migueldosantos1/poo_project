f2ed09d293cc0772d64010089b9f9e0a27554e46
ricardoplancha12 <ricardoplancha@gmail.com>
2026-06-03 11:06:32 +0100

    phase1.sh

--- project-OOP-2526/DEMO-v1/phase1-script.sh
+++ project-OOP-2526/DEMO-v1/phase1-script.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
-# Phase 1 execution script
 
+# create the output folder if needed
 mkdir -p output
 
 echo "=================================================="
-echo "Example 1 - 4 players, full game, Player 1 wins"
+echo "Example 1"
 echo "=================================================="
-java -jar project-v1.jar input/deck-1.txt input/script-1.txt 4 > output/out1.txt
-echo "Done. See output/out1.txt"
+java -jar project-v1.jar in-out-files-for-demonstration/input/ex1-deck.txt in-out-files-for-demonstration/input/ex1-script.txt 4 > output/out1.txt
+
+echo "=================================================="
+echo "Example 2"
+echo "=================================================="
+java -jar project-v1.jar in-out-files-for-demonstration/input/ex2-deck.txt in-out-files-for-demonstration/input/ex2-script.txt 4 > output/out2.txt
+
+echo "=================================================="
+echo "Example 3"
+echo "=================================================="
+java -jar project-v1.jar in-out-files-for-demonstration/input/ex3-deck.txt in-out-files-for-demonstration/input/ex3-script.txt 4 > output/out3.txt
+
+echo "=================================================="
+echo "Example 4"
+echo "=================================================="
+java -jar project-v1.jar in-out-files-for-demonstration/input/ex4-deck.txt in-out-files-for-demonstration/input/ex4-script.txt 4 > output/out4.txt
+
+echo "=================================================="
+echo "Example 5"
+echo "=================================================="
+java -jar project-v1.jar in-out-files-for-demonstration/input/ex5-deck.txt in-out-files-for-demonstration/input/ex5-script.txt 4 > output/out5.txt
+
+echo "=================================================="
+echo "All done! Outputs saved in output/"
+echo "=================================================="
